dafed0d3ff3206f62df0bdf8013111003bef13de
yash thakkar <yash.thakkar@mirraw.com>
2024-07-14 20:51:32 +0530

    Revision Notes

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,11 +2,15 @@ class BlogPostsController < ApplicationController
     def index
         # The reason we are making use of the instance variable here, is that rails knows to share those with your erb template. If we use local variables, it will clear out after the index method finishes out executing but with the instance variables, we can keep them around.
         @blog_posts = BlogPost.all
+
+        # All the blog posts will be now accessible in the views, as per the controller, the folder in the views will be blog_posts, and the template will be the same name as the name of the method. That is the index.html.erb template
     end
 
     def show
         @blog_post = BlogPost.find(params['id'])
     rescue
+
+        # The root_path here is as defined with root in the routes. root_path and root_url variables is automatically created by rails
         redirect_to root_path
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post 
+  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
   post "/blog_posts", to:"blog_posts#create", as: :blog_posts
 
   # The root keyword helps us to set the root route, before the #, we have the controller present and after the #, we have the method of the specific controller. 
-  root "blog_posts#index"
+  root "blog_posts#index" # Here, the root specifies localhost:3000, and we are attaching the controller along with the method.
 end
