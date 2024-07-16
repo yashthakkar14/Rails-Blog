3b913887c295883238ab72bac6244290fce5a5c1
yash thakkar <yash.thakkar@mirraw.com>
2024-07-16 11:55:08 +0530

    Edit and Update Blog Post Actions

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,16 +1,11 @@
 class BlogPostsController < ApplicationController
     def index
-        # The reason we are making use of the instance variable here, is that rails knows to share those with your erb template. If we use local variables, it will clear out after the index method finishes out executing but with the instance variables, we can keep them around.
         @blog_posts = BlogPost.all
-
-        # All the blog posts will be now accessible in the views, as per the controller, the folder in the views will be blog_posts, and the template will be the same name as the name of the method. That is the index.html.erb template
     end
 
     def show
         @blog_post = BlogPost.find(params['id'])
     rescue
-
-        # The root_path here is as defined with root in the routes. root_path and root_url variables is automatically created by rails
         redirect_to root_path
     end
 
@@ -19,12 +14,24 @@ class BlogPostsController < ApplicationController
     end
 
     def create
-        # @blog_post = BlogPost.new(params[:blog_post])
         @blog_post= BlogPost.new(blog_post_params)
         if @blog_post.save
           redirect_to @blog_post
         else
-            render :new, status: :unprocessable_entity #render the new action
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+        @blog_post = BlogPost.find(params[:id])
+    end
+
+    def update
+        @blog_post = BlogPost.find(params[:id])
+        if @blog_post.update(blog_post_params)
+            redirect_to @blog_post
+        else
+            render :edit, status: :unprocessable_entity
         end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
 
   get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  patch "/blog_posts/:id", to: "blog_posts#update"
+  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
   post "/blog_posts", to:"blog_posts#create", as: :blog_posts
 
-  # The root keyword helps us to set the root route, before the #, we have the controller present and after the #, we have the method of the specific controller. 
-  root "blog_posts#index" # Here, the root specifies localhost:3000, and we are attaching the controller along with the method.
+  root "blog_posts#index" 
 end
